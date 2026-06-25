29a2481ed30976830f6afd8ddcbfd78727b10902
LOKESHWARAN <2023pecai431@panimalar.ac.in>
2026-06-26 04:14:28 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -2,11 +2,11 @@ import re
 
 def clean_text(text):
 
-    text=text.lower()
+    if not text:
+        return ""
 
-    text = re.sub(r'[^a-zA-Z0-9\s]',' ',text)
-
-    text= re.sub(r'\s+',' ',text)
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z0-9\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
 
     return text.strip()
-   
